c44d63f2803b7b0e9feb6401517d9e87e5ecc917
tranbrandon1233 <70561420+tranbrandon1233@users.noreply.github.com>
2025-02-10 02:04:37 -0800

    Second lesson

--- first_node.py
+++ first_node.py
@@ -13,8 +13,8 @@ class MyNode(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    node = MyNode()
-    rclpy.spin(node)
+    ros_node = MyNode()
+    rclpy.spin(ros_node)
     rclpy.shutdown()
 
 if __name__ == '__main__':
